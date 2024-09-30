38347b9952e9b49c5f308379f5d4e538b288f14b
BodeTobias <149093216+BodeTobias@users.noreply.github.com>
2024-09-30 09:47:51 +0200

    Adding DOIs and pytest dependency

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'AutoPDEx'
 copyright = '2024, Tobias Bode'
 author = 'Tobias Bode'
-release = '0.0.2'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
